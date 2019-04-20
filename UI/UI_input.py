e79bebeb3afe70bb78832214ad7c01b7db7583e7
varunkr95g <49811466+varunkr95g@users.noreply.github.com>
2019-04-20 20:43:50 +0530

    Create UI_input.py

---
+++ UI/UI_input.py
@@ -0,0 +1,8 @@
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import pandas as pd
+
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+print(filename)
+digits = pd.read_csv(filename,skiprows=[0],header=None)# read the .csv file into a pandas dataFrame
